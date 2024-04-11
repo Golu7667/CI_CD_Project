58569743b9929ee4e5c6f1cd1b6147b84473dd06
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2024-04-11 15:31:25 +0530

    Update index.js
    update2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 
 app.get("/",(req,res)=>{
-    res.send("hi port is running on 8000 ,update")
+    res.send("hi port is running on 8000 ,update2")
 })
 
 app.listen(8000,()=>{
