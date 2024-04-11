b9c94f85941169df503623bbc20171db4a5ab7b7
unknown <goluchoudhary7808@gmail.com>
2024-04-11 12:54:17 +0530

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("hi port is running on 8000")
+})
+
+app.listen(8000,()=>{
+    console.log("server is running")
+})
