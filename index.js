d9fd89a17d7125a43dea7478decfbff120c1290d
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2024-04-12 11:56:36 +0530

    Update index.js
    seacond time update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 
 app.get("/",(req,res)=>{
-    res.send("hi port is running on 8000 & update ")
+    res.send("hi port is running on 8000 & update seacond time")
 })
 
 app.listen(8000,()=>{
