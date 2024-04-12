b83d4e25e5ba684a48c632b623dab2b071df6e0c
unknown <goluchoudhary7808@gmail.com>
2024-04-12 13:02:56 +0530

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express=require("express")
+const mongoose=require("mongoose")
 const app=express()
+
+mongoose.connect("",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("database connected")
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.get("/",(req,res)=>{
     res.send("hi port is running on 8000 & update fourth time")
