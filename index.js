e289095073ee812169d44a1c059410d28542f86e
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2024-04-12 10:45:32 +0530

    Update index.js
    change index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 
 app.get("/",(req,res)=>{
-    res.send("hi port is running on 8000 & change ")
+    res.send("hi port is running on 8000 & change & update after update ")
 })
 
 app.listen(8000,()=>{
