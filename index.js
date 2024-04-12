ae0240397fcea6f2e915875e87b40c3bbd9f7283
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2024-04-12 10:55:13 +0530

    Update index.js
    test for ci cd runner

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 
 app.get("/",(req,res)=>{
-    res.send("hi port is running on 8000 & change & update after update ")
+    res.send("hi port is running on 8000 &  update ")
 })
 
 app.listen(8000,()=>{
